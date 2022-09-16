ae80c9c51ab111a89f8c93a9cf9ec1fe39e3e447
anavitoria <anavitoriasln@gmail.com>
2022-09-16 18:58:17 -0300

    voltando as alterações funcionais

--- Projeto1.py
+++ Projeto1.py
@@ -24,7 +24,6 @@ class Usuario(BaseModel):
             nome: str
             email: str
             senha: str
-            
        
         # Classe representando a lista de endereços de um cliente
 class ListaDeEnderecosDoUsuario(BaseModel):
@@ -51,16 +50,15 @@ db_produtos = {}
 db_end = {}
 db_carrinhos = {}
 
-
 # Validar Usuários
-# def regras_cadastro_usuario(usuario):
-#     if usuario.id in db_usuarios:
-#         return FALHA
-#     if usuario.email.endswith('@') == -1:
-#         return FALHA
-#     if len(usuario.senha) < 3:
-#         return FALHA
-#     return usuario(criar_usuário)
+def regras_cadastro_usuario(usuario):
+    if usuario.id in db_usuarios:
+        return FALHA
+    if usuario.email.endswith('@') == -1:
+        return FALHA
+    if len(usuario.senha) < 3:
+        return FALHA
+    return usuario(criar_usuário)
        
         
 @app.post("/usuario/")
@@ -91,7 +89,7 @@ async def retornar_usuario_com_nome(nome):
 async def deletar_usuario(id: int):
             if id in db_usuarios:
                 del db_usuarios[id]
-                return "o usuário foi deletado do nosso banco de dados com sucesso"
+                return "o usuário foi deletado do nosso banco de da"
             
         # Se não existir usuário com o id_usuario retornar falha, 
         # senão retornar uma lista de todos os endereços vinculados ao usuário
@@ -102,8 +100,6 @@ async def deletar_usuario(id: int):
 async def retornar_enderecos_do_usuario(id_usuario: int):
             if id_usuario not in db_usuarios:
                 return FALHA
-            else:
-                db_usuarios = id_usuario[id_usuario]
         
         
         
@@ -122,14 +118,6 @@ async def retornar_emails(dominio: str):
         
         # Se não existir usuário com o id_usuario retornar falha, 
         # senão cria um endereço, vincula ao usuário e retornar OK
-# endereco = {
-#     1: {"rua":"pompeia0", "número":31, "cep":25455},
-#     2: {"rua":"pompeia1", "número":32, "cep":25455},
-#     3: {"rua":"pompeia2", "número":33, "cep":25455},
-#     4: {"rua":"pompeia3", "número":34, "cep":25455},
-#     5: {"rua":"pompeia4", "número":35, "cep":25455},
-#     }
-
 @app.post("/endereco/{id_usuario}/")
 async def criar_endereco(endereco: Endereco, id_usuario: int):
             return OK
